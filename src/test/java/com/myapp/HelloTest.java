13df3ffba463ae6d990b1b1c4119dbb718ae7bde
Rashmitha kl <rashmithakl@Rashmithas-MacBook-Air.local>
2026-04-19 17:21:17 +0530

    adding the test file

---
+++ src/test/java/com/myapp/HelloTest.java
@@ -0,0 +1,12 @@
+package com.myapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloTest {
+
+    @Test
+    void testSample() {
+        assertEquals(2, 1 + 1);
+    }
+}
